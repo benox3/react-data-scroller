c10dcd44ac8cebf4f525f15f488fef869e560f54
benox3 <ben.bj32001@gmail.com>
2019-03-12 01:47:53 -0400

    chore: add build, clean, and lint to scripts

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss'
+import pkg from './package.json'
+
+const isProd = process.env.BUILD === 'production';
+const getDestination = (dest) => {
+  if (isProd) return dest.replace('.js', '.min.js');
+  return dest;
+};
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      format: 'cjs',
+      file: getDestination(pkg.main),
+      name: 'DataScroller',
+    },
+    {
+      format: 'es',
+      file: getDestination(pkg.module),
+      name: 'DataScroller',
+    },
+  ],
+  plugins: [
+typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      plugins: []
+    })
+  ]
+}
