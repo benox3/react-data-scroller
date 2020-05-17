24e06754fec3367e660c4c8043ebd82e30cfd6ea
brackenish <bracken83@icloud.com>
2020-05-17 14:07:50 -0400

    refactor(headers): extract column rendering function

--- src/components/Headers/Headers.tsx
+++ src/components/Headers/Headers.tsx
@@ -9,11 +9,15 @@ export type Props = {
 export default function Headers({ headerHeight, columns }: Props) {
   return (
     <div style={{ display: 'flex', height: headerHeight }}>
-      {columns.map((column, index) => (
-        <div key={index} style={{ width: column.width }}>
-          {(column.headerRenderer && column.headerRenderer(column)) || <div />}
-        </div>
-      ))}
+      {columns.map(renderColumn)}
+    </div>
+  );
+}
+
+function renderColumn(column: ColumnProps, index: number) {
+  return (
+    <div key={index} style={{ width: column.width }}>
+      {(column.headerRenderer && column.headerRenderer(column)) || <div />}
     </div>
   );
 }
